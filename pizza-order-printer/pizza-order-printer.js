417d192f14918c8611e8e13224395f4fbeef0149
“janamarie-coder” <janaharms16@gmail.com>
2026-06-30 15:15:10 -0400

    add pizza order printer

--- pizza-order-printer/pizza-order-printer.js
+++ pizza-order-printer/pizza-order-printer.js
@@ -5,3 +5,23 @@ Make a Node.js app that prints pizza orders to the console.
 
 Check the README.md file for instructions.
 ------------------------- */
+
+import fs from "fs/promises";
+
+let command = process.argv[2];
+let index = Number(process.argv[3]);
+
+const data = await fs.readFile("data.json", "utf-8");
+const orders = JSON.parse(data);
+
+if (command === "getAllOrders") {
+  for (let i = 0; i < orders.length; i++) {
+    console.log(orders[i]);
+  }
+} else if (command === "getOneOrder") {
+  if (orders[index]) {
+    console.log(orders[index]);
+  } else {
+    console.log("Order not found. Please enter a valid order number.");
+  }
+}
